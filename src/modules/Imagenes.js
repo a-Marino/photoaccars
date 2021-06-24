69a48af199f7ba07a8c709672879dedfcec66235
a-Marino <andresmarinolaiolo@hotmail.com.ar>
2021-06-24 18:08:01 -0300

    Imagenes

---
+++ src/modules/Imagenes.js
@@ -0,0 +1,68 @@
+export default {
+    namespaced: true,
+    state: {
+        // Datos de prueba
+        imagenesState: [
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249214542539/19EFB30AE2C12682AB19870846376E98EAB6BC60/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Mclaren 720s GT3',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249214540666/C58335C97E8766ED816AECFF1831F9C871238979/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Acura NSX #1',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249214541266/D2F81D77B1DF58FFBA0A6CC9E693F9087744A079/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Acura NSX #2',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249214540152/35E04861468F987750F3262DBDF5461AE90C110A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Acura NSX #3',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249214539471/F1661C9A499AC81567AACD93DF2F4A8A1113B810/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Acura NSX #4',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249213474176/B92B9BFA9A693661E19B7B5047934C38D6D9B0E5/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Ferrari Dino',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249213220576/C14D8DC360A27E28C9A083D37417B8EDDFFDC990/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Charles Leclerc at Austria',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249212728444/542DD9075BB656ABF7DB0B23C98FEF727B7E10AE/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Lando Norris at Austria',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249212723670/79ACCC94A318480B706974318B826F2E010129D7/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Mitsubishi Lancer GT500',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249212675761/0A48CF35EF41E1AF80218D5BFC0A99AFE936E29A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Mitsubishi Lancer GT500',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249212387130/FD3F51D04701C602A299EF86BD402CDFC12D61F5/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Ferrari Dino',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249212378173/FA5E6C768DA1FF684545036491F1AC668FB2DCF5/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Ferrari Dino',
+            },
+            {
+                imgPath: 'https://steamuserimages-a.akamaihd.net/ugc/1686017249209650243/A5002CD516262FF43FDECFCFDB8B7885AF3DB8D8/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+                title: 'Ferrari 288 GTO',
+            },
+        ],
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+}
--- src/views/Slider.vue
+++ src/views/Slider.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-        Slider
+    <div class="slider">
+        <div v-for='(imagen, index) in imagenesState' :key='index' :style="`background-image:url(${imagen.imgPath})`" class="slider-content">
+        </div>
     </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
     name: 'Slider',
+    computed: {
+        ...mapState('imagenes', ['imagenesState']),
+    }
 }
 </script>
 
 <style scoped>
+    .slider {
+        height: 100vh;
+        scroll-snap-type: y mandatory;
+        overflow-y: scroll;
+    }
 
+    .slider-content{
+        height: 100vh;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        scroll-snap-align: center;
+    }
 </style>
